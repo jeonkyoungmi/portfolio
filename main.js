827aa909371a2a01946d654f13041f5a69dc4da2
HANSSEMAD\2019Z088 <glad64@naver.com>
2021-04-13 14:03:41 +0900

    JS:Navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// navbar 메뉴 클릭시 스크롤링
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const link = event.target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    console.log(link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
