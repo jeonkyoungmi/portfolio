22cbc002ed879abebcb11c2410e34910e05d0c50
HANSSEMAD\2019Z088 <glad64@naver.com>
2021-04-13 14:58:58 +0900

    JS:ContactMe

--- main.js
+++ main.js
@@ -13,14 +13,20 @@ document.addEventListener('scroll', () => {
 });
 
 // navbar 메뉴 클릭시 스크롤링
-
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const link = event.target.dataset.link;
-    if(link == null) {
-        return;
-    }
-    console.log(link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    if(link == null) return;
+    scrollIntoView(link);
 });
+
+// contact me 클릭시 contact 화면으로
+const cntctBtn = document.querySelector('.home__contact');
+cntctBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
